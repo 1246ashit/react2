96f6cb807f1afe42d259d313002a8c896be37571
jj <1246ashit@gmail.com>
2024-05-03 20:32:48 +0800

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {Home,About,Contact,Projects} from './pages'
+import { NavBar } from "./componets/NavBar";
 
 
-function App() {
+
+function App  ()  {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <main className='bg-slate-300/20'>
+      <Router>
+        <NavBar/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/projects' element={<Projects/>} />
+          <Route path='/contact' element={<Contact/>} />
+        </Routes>
+      </Router>
+    </main>
+  );
+};
 
-export default App
+export default App;
