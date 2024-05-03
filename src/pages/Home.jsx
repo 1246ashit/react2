465b7723970552b9b9a9908059af03dc9e73ba8a
jj <1246ashit@gmail.com>
2024-05-03 22:38:08 +0800

    建出城堡

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import {React,Suspense} from 'react'
+import { Canvas } from '@react-three/fiber'
+import Loader from '../componets/Loader'
+import Island from '../models/Island'
 
 const Home = () => {
+  const adjustIslandForScreenSize=()=>{
+    let screenScale=null;
+    let screenPostion=[0,-6.5,-43];
+    let rotation=[0.1,4.7,0];
+    if(window.innerWidth<768){
+      screenScale=[0.9,0.9,0.9]
+    }
+    else{
+      screenScale=[1,1,1]
+    }
+    return [screenScale,screenPostion,rotation]
+  }
+  const[islandScale,islandPosition,islandRotation]=adjustIslandForScreenSize();
+
   return (
-    <div>Home</div>
+    <section className='w-full h-screen relative'>
+      <Canvas className='w-full h-screen bg-transparent'
+        camera={{ near: 0.1, far: 100 }}
+        >
+          <Suspense fallback={<Loader/>}>
+            <directionalLight/>
+            <ambientLight/>
+            <pointLight/>
+            <spotLight/>
+            <hemisphereLight></hemisphereLight>
+            <Island 
+              position={islandPosition} 
+              scale={islandScale} 
+              rotation={islandRotation}
+            />
+          </Suspense>
+      </Canvas>
+    </section>
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude:['**/*.glb'],
   base:"/react2/",
   server: {
     host: '0.0.0.0'
-}
+},
 })
